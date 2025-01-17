6b90f9c4607278da65ae243251f954345dd8faaf
Mikyas59 <113638977+Mikyas59@users.noreply.github.com>
2025-01-17 17:15:50 -0500

    created a few options for mapping.

--- prep_data.R
+++ prep_data.R
@@ -1,7 +1,10 @@
+
 library(dplyr)
+library(readr)
 library(collapse)
 library(data.table)
 library(stringr)
+source("my_functions.R")
 
 final_comp <- fst::read_fst("data/final_comp.fst")
 
@@ -93,7 +96,7 @@ ntee <- read.table("data/ntee_cd.txt", sep = "-") %>%
   mutate(ntee_cd = str_squish(ntee_cd))
 
 
-## individuals with the highest compensation 
+## individuals with the highest compensation -- doesn't align with the purpose of application
 # final_comp %>% 
 #   arrange(desc(rep_comp)) %>% 
 #   fselect(names, title, rep_comp, org_name, org_type, comb_501c, ntee_cd, 
@@ -161,8 +164,6 @@ font_add_google("Sen", "Sen")
 
 showtext_auto()
 
-
-set.seed(20250113) 
 
 
 q99 <- final_comp %>% 
@@ -251,14 +252,222 @@ test + inset_element(p_legend, l = 0.6, r = 1.0,
                      t = 0.99, b = 0.7, clip = FALSE)
  
   
+library(highcharter)
 
-final_comp %>% 
+
+dta <- download_map_data("https://code.highcharts.com/mapdata/countries/us/us-all.js")
+get_data_from_map(dta)
+
+
+df_rel <- final_comp %>%
+  fselect(title, rep_comp, state, avg_hrs) %>% 
   fsubset(title == common_roles$title[1]) %>% 
-  fsummarise(mean = mean(rep_comp, na.rm = T), 
-             median = median(rep_comp, na.rm = T), 
-             qx = quantile(rep_comp, probs = 0.99),
-             min = min(rep_comp), 
-             max = max(rep_comp))
+  fsubset(rep_comp < q99) %>% 
+  fgroup_by(state) %>% 
+  fsummarise(med_comp = median(rep_comp, na_rm = TRUE), 
+             q25 = quantile(rep_comp, probs = 0.25), 
+             q75 = quantile(rep_comp, probs = 0.75))
+
+
+library(urbnmapr)
+
+ac <- get_urbn_map(map = "states")
+ac_labels <- get_urbn_labels(map = "states")
+
+
+  ab <-
+    df_rel %>% 
+    # filter(title == common_roles$title[1]) %>% 
+    group_by(state) %>% 
+    add_count() %>% 
+    filter(n>=5) %>% 
+    summarise(avg_comp = round(mean(rep_comp, na.rm = T)), 
+              avg_hrs = round(mean(avg_hrs, na.rm = T))) %>% 
+    mutate(hourly_comp = avg_comp/avg_hrs) %>% 
+    mutate(FT_comp = round(hourly_comp*40)) %>% 
+    rename(state_abbv = state, 
+           Compensation = FT_comp) %>%
+    full_join(ac %>% 
+                select(lat, long, state_abbv, group), by = "state_abbv") %>% 
+    ggplot()+
+    geom_polygon(aes(long, lat, group = group, fill = Compensation), 
+                 color = "#ffffff", linewidth = 0.25) +
+    
+    scale_fill_gradient2(
+      labels = scales::dollar,
+      low = "#DEF5E5FF",
+      mid = "#3487A6FF",
+      high = "#0B0405FF",
+      midpoint = median(df_rel$rep_comp), 
+      na.value = "gray80"
+    ) +
+    
+    labs("Compensation") +
+    xlab(NULL)+
+    ylab(NULL)+
+    custom_style3()+
+    theme(
+      legend.position = "top",
+      legend.justification = 0.5,
+      legend.key.width = unit(3, "cm"),
+      panel.grid.major = element_blank(), 
+      panel.grid.minor = element_blank(), 
+      axis.text = element_blank(), 
+      plot.title = element_text(face = "bold")
+      
+    )+
+    
+    labs(title = paste0("Mean Compensation for ED's by State"), 
+         caption = "Note: The graph does not include the top 0.01% of earners.
+         Source: Internal Revenue Service 2024")
+  
+
+  
+plotly::ggplotly(ab) %>% 
+  plotly::layout(
+    font = list(
+      family = "AgencyFB"
+    ), 
+    legend = list(orientaion = 'h')
+  ) ## too basic not customizable 
+  
+  
+states <- sf::read_sf(
+  "https://rstudio.github.io/leaflet/json/us-states.geojson")
+
+
+
+states <- states %>% 
+  left_join((cbind.data.frame(
+    state.abb, state.name
+  ) %>% rename(name = 2)), 
+  by = "name") %>% 
+  mutate(state.abb = case_when(
+    name == "District of Columbia" ~ "DC", 
+    name == "Puerto Rico" ~ "PR", 
+    TRUE ~ state.abb
+  )) %>% 
+  rename(state = state.abb) %>% 
+  left_join(df_rel, by = "state")
+  
+  
+  
+  
+  
+  
+  
+library(leaflet)
+
+m <- leaflet(states) %>%
+  setView(-96, 40, 4.7) %>%
+  addTiles("MapBox", options = providerTileOptions(
+    id = "mapbox.light"))
+
+my_pal <- c("#A0DFB9FF",
+           "#3487A6FF","#3671A0FF", "#3A599AFF",
+           "#414286FF", "#3C3163FF",
+           "#312141FF", "#201322FF")
+
+
+bins <- c(quantile(df_rel$med_comp, probs = 0.0),
+          quantile(df_rel$med_comp, probs = 0.25), 
+          quantile(df_rel$med_comp, probs = 0.50),
+          quantile(df_rel$med_comp, probs = 0.75),
+          quantile(df_rel$med_comp, probs = 0.95),
+          Inf)
+pal <- colorBin(my_pal, domain = states$med_comp, bins = bins)
+
+
+
+mylabels <- paste(
+  "State: ", states$name, "<br/>",
+  "Median compensation: ", scales::dollar(states$med_comp), "<br/>",
+  "Half fall between: ", scales::dollar(states$q25), "&", scales::dollar(states$q75)
+) %>%
+  lapply(htmltools::HTML)
+
+htmltitle <- "<h5> Compensation for Executive Directors by State</h5>"
+htmlcap <- "<h5> Source: Internal Revenue Service 2024</h5>"
+
+m %>% addPolygons(
+  fillColor = ~pal(med_comp),
+  weight = 2,
+  opacity = 1,
+  color = "white",
+  dashArray = "3",
+  fillOpacity = 0.7, 
+  label = mylabels,
+  labelOptions = labelOptions( 
+    style = list("font-weight" = "normal", padding = "3px 8px"), 
+    textsize = "13px", 
+    direction = "auto"
+  ))%>%
+  addLegend( pal= pal, 
+             values=~med_comp, 
+             opacity=0.7, 
+             title = NULL, 
+             position = "bottomleft" 
+  ) %>%
+  addControl(html=htmltitle, position = "topright") %>% 
+  addControl(html=htmlcap, position = "bottomright")
+
+# us_value_shifted <- us_value %>%
+#   shift_geometry(position = "outside") %>%
+#   mutate(tooltip = paste(NAME, estimate, sep = ": "))
+
+
+gg <- ggplot(states, aes(fill = med_comp)) + 
+  geom_sf_interactive(aes(tooltip = med_comp, data_id = name), 
+                      size = 0.1) + 
+  scale_fill_viridis_c(option = "plasma", labels = label_dollar()) + 
+  labs(title = "Median housing value by State, 2019",
+       caption = "Data source: 2019 1-year ACS, US Census Bureau",
+       fill = "ACS estimate") + 
+  custom_style3() +
+  theme(axis.text = element_blank()) 
+
+girafe(ggobj = gg) %>%
+  girafe_options(opts_hover(css = "fill:cyan;"), 
+                 opts_zoom(max = 9))
+
+
+
+highcharter::hcmap(
+  map = "countries/us/us-all",
+  data = df_rel,
+  value = "med_comp",
+  joinBy = c("hc-a2", "state"),
+  name = "Median Compensation",
+  dataLabels = list(enabled = TRUE,
+                    format = "{point.name}"
+                    ),
+  tooltip = list(
+    # "{point.name} : {point.med_comp} <br>
+    # {Bottom 25} : {point.q25} <br>
+    # {Top 25 above}: {point.q75}",
+    
+    
+
+    valueDecimals = 0,
+    valuePrefix = "$",
+    valueSuffix = "USD"
+  )
+
+) |>
+  
+  highcharter::hc_colorAxis(
+    stops = highcharter::color_stops(colors = c("#DEF5E5FF", "#A0DFB9FF",
+                                                "#3487A6FF","#3671A0FF", "#3A599AFF",
+                                                "#414286FF", "#3C3163FF",
+                                                "#312141FF", "#201322FF", "#0B0405FF"
+
+    ))
+
+  ) |>
+  highcharter::hc_title(text = paste0("Compensation Distribution for EDs by State")) |>
+  highcharter::hc_caption(text = "Source: Internal Revenue Service 2024",
+                          align = "right")
+
  
 
 
