cf8fbce3e9df25b4388602bdfab1b28aef2508de
Mikyas59 <113638977+Mikyas59@users.noreply.github.com>
2025-01-22 17:39:00 -0500

    preparing functions for graphs and data manipulation for app.

--- prep_data.R
+++ prep_data.R
@@ -1,4 +1,6 @@
 
+## prepare app data frames, functions and experiment with different mapping packages. 
+
 library(dplyr)
 library(readr)
 library(collapse)
@@ -164,313 +166,229 @@ font_add_google("Sen", "Sen")
 
 showtext_auto()
 
+gdtools::register_gfont("Sen")
 
 
-q99 <- final_comp %>% 
-  fsubset(title == common_roles$title[1]) %>%
-  fsummarise(qx = quantile(rep_comp, probs = 0.99)) %>% 
-  as.double()
+## add a value box to show the overall median value. 
 
-test <- 
-final_comp %>% 
-  fsubset(title == common_roles$title[1] &
-            !is.na(asset2) &
-            rep_comp <= q99) %>%
-  fselect(asset2, rep_comp, org_name, label) %>% 
-  fmutate(med_comp = median(rep_comp)) %>% 
-  # slice_sample(n = 10000, replace = FALSE) %>% 
-  ggplot(aes(y = rep_comp, x = fct_rev(asset2)))+
-  stat_halfeye(alpha = 0.5)+
-  stat_interval()+
-  stat_summary(geom = "point", fun = median, size =3) +
-  geom_hline(aes(yintercept = med_comp), lty = "dashed")+
-  # facet_wrap(~asset2, ncol = 1,
-  #            scales = "free_y")+
-  custom_style3()+
-  scale_y_continuous(labels = scales::dollar,
-                     name = NULL, 
-                     expand = c(0,0))+
-    scale_x_discrete(name = NULL, labels = function(i) custom_lbl_wrap(i,w=19))+
-  scale_color_manual(values = wes_chevalier)+
-  labs(title = "Compensation for position by organization asset size", 
-       caption = "Source: Internal Revenue Service 2024")+
-  coord_flip(clip = 'off')+
-  theme(panel.grid.major.y = element_line())
+plot_dist(1)
+  
+
+
+
+# library(highcharter)
+# 
+# 
+# dta <- download_map_data("https://code.highcharts.com/mapdata/countries/us/us-all.js")
+# get_data_from_map(dta)
+
+
+# 
+# library(urbnmapr)
+# 
+# ac <- get_urbn_map(map = "states")
+# ac_labels <- get_urbn_labels(map = "states")
+# 
+# 
+#   ab <-
+#     df_rel %>% 
+#     # filter(title == common_roles$title[1]) %>% 
+#     # group_by(state) %>% 
+#     # add_count() %>% 
+#     # filter(n>=5) %>% 
+#     # summarise(avg_comp = round(mean(rep_comp, na.rm = T)), 
+#     #           avg_hrs = round(mean(avg_hrs, na.rm = T))) %>% 
+#     # mutate(hourly_comp = avg_comp/avg_hrs) %>% 
+#     # mutate(FT_comp = round(hourly_comp*40)) %>% 
+#     rename(state_abbv = state
+#            # ,
+#            # Compensation = FT_comp
+#            ) %>%
+#     full_join(ac %>% 
+#                 select(lat, long, state_abbv, group), by = "state_abbv") %>% 
+#     ggplot()+
+#     geom_polygon_interactive(aes(long, lat, group = group, fill = med_comp, 
+#                                  tooltip = med_comp, data_id = state_abbv), 
+#                  color = "#ffffff", linewidth = 0.25) +
+#     
+#     scale_fill_gradient2(
+#       labels = scales::dollar,
+#       low = "#DEF5E5FF",
+#       mid = "#3487A6FF",
+#       high = "#0B0405FF",
+#       midpoint = median(df_rel$med_comp), 
+#       na.value = "gray80"
+#     ) +
+#     custom_style3()+
+#     theme(
+#       legend.position = "top",
+#       legend.justification = 0.5,
+#       legend.key.width = unit(3, "cm"),
+#       panel.grid.major = element_blank(), 
+#       panel.grid.minor = element_blank(), 
+#       axis.text = element_blank(), 
+#       plot.title = element_text(face = "bold")
+#       
+#     )+
+#     
+#     labs(title = paste0("Mean Compensation for ED's by State"), 
+#          caption = "Note: This graph does not include the top 0.01% of earners.
+#          Source: Internal Revenue Service 2024", 
+#          x = NULL, y = NULL, fill = NULL)
+  
+## urbn doesn't work with interactive features. good for static mapping however. 
+# girafe(ggobj = ab)%>%
+#   girafe_options(opts_hover(css = "fill:cyan;"), 
+#                  opts_zoom(max = 3))
+
+  
+  
+# plotly::ggplotly(ab) %>% 
+#   plotly::layout(
+#     font = list(
+#       family = "AgencyFB"
+#     ), 
+#     legend = list(orientaion = 'h')
+#   ) ## too basic not customizable 
+  
+  
+# states <- sf::read_sf(
+#   "https://rstudio.github.io/leaflet/json/us-states.geojson")
+# 
+# 
+# 
+# states <- states %>% 
+#   left_join((cbind.data.frame(
+#     state.abb, state.name
+#   ) %>% rename(name = 2)), 
+#   by = "name") %>% 
+#   mutate(state.abb = case_when(
+#     name == "District of Columbia" ~ "DC", 
+#     name == "Puerto Rico" ~ "PR", 
+#     TRUE ~ state.abb
+#   )) %>% 
+#   rename(state = state.abb) %>% 
+#   left_join(df_rel, by = "state")
+  
+  
+us_geo <- tigris::states(class = "sf", cb = TRUE) %>% 
+       tigris::shift_geometry()
+  
+
+us_geo <- us_geo %>% 
+  rename(state = STUSPS) %>% 
+  left_join(make_df_rel(1), by = "state") %>% 
+  filter(state %in% c(state.abb, "DC", "PR")) %>% 
+  mutate(across(c("med_comp", "q25", "q75"), round))
+
+
  
-
-set.seed(20250115)
-df_legend <- final_comp %>% 
-  fselect(rep_comp) %>% 
-  slice_sample(n=1000, replace = F) %>% 
-  fsubset(rep_comp<=400000)
-
-
-library(ggtext)
-p_legend <-
-  df_legend %>% 
-  ggplot(aes(y=rep_comp, x = 1)) +
-  stat_halfeye(fill_type = "segments", alpha = 0.5) +
-  stat_interval() +
-  stat_summary(geom = "point", fun = median, size =3) +
-  annotate(
-    "text",
-    x = c(0.8, 0.8, 0.8, 1.4, 1.75),
-    y = c(60000, 270000, 180000, 92000, 150000),
-    label = c("50 % of prices\nfall within this range", "95 % of prices", 
-              "80 % of prices", "Median", "Distribution of prices"),
-    family = "Sen", size = 3, vjust = 1
-  ) +
-   geom_curve(
-    data = data.frame(
-      x = c(0.8, 0.80, 0.80, 1.3, 1.7),
-      xend = c(0.95, 0.95, 0.95, 1.075, 1.7), 
-      y = c(60000, 270000, 180000, 92000, 87000),
-      yend = c(60000, 270000, 180000, 92000, 10000)),
-    aes(x = x, xend = xend, y = y, yend = yend),
-    stat = "unique", curvature = -0.2, size = 0.2, color = "black",
-    arrow = arrow(angle = 20, length = unit(2, "mm")), 
-    linewidth = 1.05
-  ) +
-  scale_color_manual(values = wes_chevalier) +
-  coord_flip(xlim = c(0.5, 1.85),
-             ylim = c(0, 380000), expand = TRUE) +
-  guides(color = "none") +
-  labs(title = "Legend") +
-  custom_style3()+
-  theme_void(base_family = "Sen")+ 
-  theme(plot.title = element_text(family = "Sen", size = 12,
-                                  hjust = 0.05),
-        plot.background = element_rect(color = "grey30",
-                                       size = 0.2))
   
   
- 
+# library(leaflet)
+# 
+# m <- leaflet(states) %>%
+#   setView(-96, 40, 4.7) %>%
+#   addTiles("MapBox", options = providerTileOptions(
+#     id = "mapbox.light"))
+# 
+# my_pal <- c("#A0DFB9FF",
+#            "#3487A6FF","#3671A0FF", "#3A599AFF",
+#            "#414286FF", "#3C3163FF",
+#            "#312141FF", "#201322FF")
 
 
-library(patchwork)
-test + inset_element(p_legend, l = 0.6, r = 1.0,
-                     t = 0.99, b = 0.7, clip = FALSE)
- 
-  
-library(highcharter)
+my_pal2 <- c("#DEF5E5FF", "#A0DFB9FF",
+             "#3487A6FF","#3671A0FF", "#3A599AFF",
+             "#414286FF", "#3C3163FF",
+             "#312141FF", "#201322FF", "#0B0405FF")
 
 
-dta <- download_map_data("https://code.highcharts.com/mapdata/countries/us/us-all.js")
-get_data_from_map(dta)
-
-
-df_rel <- final_comp %>%
-  fselect(title, rep_comp, state, avg_hrs) %>% 
-  fsubset(title == common_roles$title[1]) %>% 
-  fsubset(rep_comp < q99) %>% 
-  fgroup_by(state) %>% 
-  fsummarise(med_comp = median(rep_comp, na_rm = TRUE), 
-             q25 = quantile(rep_comp, probs = 0.25), 
-             q75 = quantile(rep_comp, probs = 0.75))
-
-
-library(urbnmapr)
-
-ac <- get_urbn_map(map = "states")
-ac_labels <- get_urbn_labels(map = "states")
-
-
-  ab <-
-    df_rel %>% 
-    # filter(title == common_roles$title[1]) %>% 
-    group_by(state) %>% 
-    add_count() %>% 
-    filter(n>=5) %>% 
-    summarise(avg_comp = round(mean(rep_comp, na.rm = T)), 
-              avg_hrs = round(mean(avg_hrs, na.rm = T))) %>% 
-    mutate(hourly_comp = avg_comp/avg_hrs) %>% 
-    mutate(FT_comp = round(hourly_comp*40)) %>% 
-    rename(state_abbv = state, 
-           Compensation = FT_comp) %>%
-    full_join(ac %>% 
-                select(lat, long, state_abbv, group), by = "state_abbv") %>% 
-    ggplot()+
-    geom_polygon(aes(long, lat, group = group, fill = Compensation), 
-                 color = "#ffffff", linewidth = 0.25) +
-    
-    scale_fill_gradient2(
-      labels = scales::dollar,
-      low = "#DEF5E5FF",
-      mid = "#3487A6FF",
-      high = "#0B0405FF",
-      midpoint = median(df_rel$rep_comp), 
-      na.value = "gray80"
-    ) +
-    
-    labs("Compensation") +
-    xlab(NULL)+
-    ylab(NULL)+
-    custom_style3()+
-    theme(
-      legend.position = "top",
-      legend.justification = 0.5,
-      legend.key.width = unit(3, "cm"),
-      panel.grid.major = element_blank(), 
-      panel.grid.minor = element_blank(), 
-      axis.text = element_blank(), 
-      plot.title = element_text(face = "bold")
-      
-    )+
-    
-    labs(title = paste0("Mean Compensation for ED's by State"), 
-         caption = "Note: The graph does not include the top 0.01% of earners.
-         Source: Internal Revenue Service 2024")
-  
-
-  
-plotly::ggplotly(ab) %>% 
-  plotly::layout(
-    font = list(
-      family = "AgencyFB"
-    ), 
-    legend = list(orientaion = 'h')
-  ) ## too basic not customizable 
-  
-  
-states <- sf::read_sf(
-  "https://rstudio.github.io/leaflet/json/us-states.geojson")
+# bins <- c(quantile(df_rel$med_comp, probs = 0.0),
+#           quantile(df_rel$med_comp, probs = 0.25), 
+#           quantile(df_rel$med_comp, probs = 0.50),
+#           quantile(df_rel$med_comp, probs = 0.75),
+#           quantile(df_rel$med_comp, probs = 0.95),
+#           Inf)
+# pal <- colorBin(my_pal, domain = states$med_comp, bins = bins)
 
 
 
-states <- states %>% 
-  left_join((cbind.data.frame(
-    state.abb, state.name
-  ) %>% rename(name = 2)), 
-  by = "name") %>% 
-  mutate(state.abb = case_when(
-    name == "District of Columbia" ~ "DC", 
-    name == "Puerto Rico" ~ "PR", 
-    TRUE ~ state.abb
-  )) %>% 
-  rename(state = state.abb) %>% 
-  left_join(df_rel, by = "state")
-  
-  
-  
-  
-  
-  
-  
-library(leaflet)
-
-m <- leaflet(states) %>%
-  setView(-96, 40, 4.7) %>%
-  addTiles("MapBox", options = providerTileOptions(
-    id = "mapbox.light"))
-
-my_pal <- c("#A0DFB9FF",
-           "#3487A6FF","#3671A0FF", "#3A599AFF",
-           "#414286FF", "#3C3163FF",
-           "#312141FF", "#201322FF")
-
-
-bins <- c(quantile(df_rel$med_comp, probs = 0.0),
-          quantile(df_rel$med_comp, probs = 0.25), 
-          quantile(df_rel$med_comp, probs = 0.50),
-          quantile(df_rel$med_comp, probs = 0.75),
-          quantile(df_rel$med_comp, probs = 0.95),
-          Inf)
-pal <- colorBin(my_pal, domain = states$med_comp, bins = bins)
-
-
-
-mylabels <- paste(
-  "State: ", states$name, "<br/>",
-  "Median compensation: ", scales::dollar(states$med_comp), "<br/>",
-  "Half fall between: ", scales::dollar(states$q25), "&", scales::dollar(states$q75)
-) %>%
-  lapply(htmltools::HTML)
-
-htmltitle <- "<h5> Compensation for Executive Directors by State</h5>"
-htmlcap <- "<h5> Source: Internal Revenue Service 2024</h5>"
-
-m %>% addPolygons(
-  fillColor = ~pal(med_comp),
-  weight = 2,
-  opacity = 1,
-  color = "white",
-  dashArray = "3",
-  fillOpacity = 0.7, 
-  label = mylabels,
-  labelOptions = labelOptions( 
-    style = list("font-weight" = "normal", padding = "3px 8px"), 
-    textsize = "13px", 
-    direction = "auto"
-  ))%>%
-  addLegend( pal= pal, 
-             values=~med_comp, 
-             opacity=0.7, 
-             title = NULL, 
-             position = "bottomleft" 
-  ) %>%
-  addControl(html=htmltitle, position = "topright") %>% 
-  addControl(html=htmlcap, position = "bottomright")
+# mylabels <- paste(
+#   "State: ", states$name, "<br/>",
+#   "Median compensation: ", scales::dollar(states$med_comp), "<br/>",
+#   "Half fall between: ", scales::dollar(states$q25), "&", scales::dollar(states$q75)
+# ) %>%
+#   lapply(htmltools::HTML)
+# 
+# htmltitle <- "<h5> Compensation for Executive Directors by State</h5>"
+# htmlcap <- "<h5> Source: Internal Revenue Service 2024</h5>"
+# 
+# m %>% addPolygons(
+#   fillColor = ~pal(med_comp),
+#   weight = 2,
+#   opacity = 1,
+#   color = "white",
+#   dashArray = "3",
+#   fillOpacity = 0.7, 
+#   label = mylabels,
+#   labelOptions = labelOptions( 
+#     style = list("font-weight" = "normal", padding = "3px 8px"), 
+#     textsize = "13px", 
+#     direction = "auto"
+#   ))%>%
+#   addLegend( pal= pal, 
+#              values=~med_comp, 
+#              opacity=0.7, 
+#              title = NULL, 
+#              position = "bottomleft" 
+#   ) %>%
+#   addControl(html=htmltitle, position = "topright") %>% 
+#   addControl(html=htmlcap, position = "bottomright")
 
 # us_value_shifted <- us_value %>%
 #   shift_geometry(position = "outside") %>%
 #   mutate(tooltip = paste(NAME, estimate, sep = ": "))
 
 
-gg <- ggplot(states, aes(fill = med_comp)) + 
-  geom_sf_interactive(aes(tooltip = med_comp, data_id = name), 
+
+
+
+library(ggiraph)
+gg <- ggplot(make_df_rel(1),
+             aes(fill = med_comp)) + 
+  geom_sf_interactive(aes(tooltip = paste0(NAME,"<br>",
+                                          "Median compensation: ", scales::dollar(med_comp), "<br>", 
+                                           "Half earn between: ", scales::dollar(q25), " & ",
+                                          scales::dollar(q75)),
+                          data_id = NAME), 
                       size = 0.1) + 
-  scale_fill_viridis_c(option = "plasma", labels = label_dollar()) + 
-  labs(title = "Median housing value by State, 2019",
-       caption = "Data source: 2019 1-year ACS, US Census Bureau",
-       fill = "ACS estimate") + 
+  scale_fill_gradientn(colors = my_pal2, labels = scales::label_dollar())+
+  labs(title = paste0("Median Compensation for ",
+                      common_roles$title[1], "s ", "by State"),
+       caption = "Note: This graph doesn't include the top 0.01% of earners.\nSource: Internal Revenue Service 2024",
+       fill = NULL) + 
   custom_style3() +
-  theme(axis.text = element_blank()) 
+  theme(axis.text = element_blank(), 
+        legend.position = "top",
+        legend.justification = c("left", "top"),
+        legend.key.width = unit(4, "line")
+        ) 
+
+
+
 
 girafe(ggobj = gg) %>%
   girafe_options(opts_hover(css = "fill:cyan;"), 
-                 opts_zoom(max = 9))
+                 opts_zoom(min = 0.5, max = 4))
 
 
 
-highcharter::hcmap(
-  map = "countries/us/us-all",
-  data = df_rel,
-  value = "med_comp",
-  joinBy = c("hc-a2", "state"),
-  name = "Median Compensation",
-  dataLabels = list(enabled = TRUE,
-                    format = "{point.name}"
-                    ),
-  tooltip = list(
-    # "{point.name} : {point.med_comp} <br>
-    # {Bottom 25} : {point.q25} <br>
-    # {Top 25 above}: {point.q75}",
-    
-    
 
-    valueDecimals = 0,
-    valuePrefix = "$",
-    valueSuffix = "USD"
-  )
+## functions are mostly ready. may need some minor modifications later one. 
+## save data file that has been cleaned. 
 
-) |>
-  
-  highcharter::hc_colorAxis(
-    stops = highcharter::color_stops(colors = c("#DEF5E5FF", "#A0DFB9FF",
-                                                "#3487A6FF","#3671A0FF", "#3A599AFF",
-                                                "#414286FF", "#3C3163FF",
-                                                "#312141FF", "#201322FF", "#0B0405FF"
-
-    ))
-
-  ) |>
-  highcharter::hc_title(text = paste0("Compensation Distribution for EDs by State")) |>
-  highcharter::hc_caption(text = "Source: Internal Revenue Service 2024",
-                          align = "right")
-
- 
-
-
+fst::write_fst(final_comp, "data/final_comp2.fst")
 
 
 
---
+++ prep_functions.R
@@ -0,0 +1,278 @@
+## prepare functions for app. 
+library(showtext)
+library(collapse)
+library(ggplot2)
+library(ggtext)
+library(patchwork)
+
+font_add_google("Sen", "Sen")
+
+showtext_auto()
+
+gdtools::register_gfont("Sen")
+
+## my ggplot2 theme. 
+custom_style3 <- function() {
+  font <- "Sen"
+  
+  ggplot2::theme(
+    
+    plot.title = ggplot2::element_text(family=font,
+                                       size=16,
+                                       face="bold",
+                                       color="#222222"),
+    plot.title.position = "plot",
+    plot.subtitle = ggplot2::element_text(family=font,
+                                          size=15,
+                                          margin=ggplot2::margin(9,0,9,0)),
+    legend.position = "none",
+    legend.background = ggplot2::element_blank(),
+    
+    legend.text = ggplot2::element_text(family=font,
+                                        size=14,
+                                        color="#222222", 
+                                        hjust = 0),
+    
+    
+    axis.text = ggplot2::element_text(family=font,
+                                      size=14,
+                                      color="#222222"),
+    axis.ticks = ggplot2::element_blank(),
+    axis.line = ggplot2::element_blank(),
+    
+   
+    panel.background = ggplot2::element_blank(),
+    
+    strip.background = ggplot2::element_rect(fill="white"),
+    strip.text = ggplot2::element_text(size  = 22,  hjust = 0, 
+                                       family = font), 
+    
+    plot.caption = element_text(family = font, 
+                                size = 13)
+  )
+}
+
+
+
+add_pct_sign <- function(x){
+  
+  scales::percent(x, 
+                  scale = 1)
+  
+  
+}
+
+
+custom_lbl_wrap <- function(x, w = 20){
+  
+  str_wrap(x, width = w)
+}
+
+
+
+## color palettes from Wes Anderson package. 
+
+wes_chevalier <- c("#446455FF", "#FDD262FF", "#D3DDDCFF", "#C7B19CFF")
+
+wes_moonrise2 <- c("#798E87FF", "#C27D38FF", "#CCC591FF", "#29211FFF")
+
+
+my_pal2 <- c("#DEF5E5FF", "#A0DFB9FF",
+             "#3487A6FF","#3671A0FF", "#3A599AFF",
+             "#414286FF", "#3C3163FF",
+             "#312141FF", "#201322FF", "#0B0405FF")
+
+## calculate quantiles. 
+qx_calc <- function(y, z=999){
+  
+  ## Return the 99.9th percentile if unspecified. 
+  ## Return 99.5th percentile if asked for 99.5th percentile
+  
+  vals <- final_comp %>% 
+    fsubset(title == common_roles$title[y]) %>%
+    fsummarise(q999 = quantile(rep_comp, probs = 0.999), 
+               q995 = quantile(rep_comp, probs = 0.995))   
+  
+  q999 <- vals$q999
+  
+  q995 <- vals$q995
+  
+  if(z==999) {
+    return(as.double(q999))
+  } else if(z==995){
+    return(as.double(q995))
+  }
+
+  
+ }
+
+
+## create dataframe for legend plot. 
+set.seed(20250115)
+df_legend <- final_comp %>% 
+  fselect(rep_comp) %>% 
+  slice_sample(n=1000, replace = F) %>% 
+  fsubset(rep_comp<=400000)
+
+
+
+## legend for distribution plot. no need to construct any other legend. 
+
+p_legend <-
+  df_legend %>% 
+  ggplot(aes(y=rep_comp, x = 1)) +
+  stat_halfeye(fill_type = "segments", alpha = 0.5) +
+  stat_interval() +
+  stat_summary(geom = "point", fun = median, size =3) +
+  annotate(
+    "text",
+    x = c(0.8, 0.8, 0.8, 1.4, 1.75),
+    y = c(60000, 270000, 180000, 92000, 150000),
+    label = c("50 % of prices\nfall within this range", "95 % of prices", 
+              "80 % of prices", "Median", "Distribution of prices"),
+    family = "Sen", size = 3, vjust = 1
+  ) +
+  geom_curve(
+    data = data.frame(
+      x = c(0.8, 0.80, 0.80, 1.3, 1.7),
+      xend = c(0.95, 0.95, 0.95, 1.075, 1.7), 
+      y = c(60000, 270000, 180000, 92000, 87000),
+      yend = c(60000, 270000, 180000, 92000, 10000)),
+    aes(x = x, xend = xend, y = y, yend = yend),
+    stat = "unique", curvature = -0.2, size = 0.2, color = "black",
+    arrow = arrow(angle = 20, length = unit(2, "mm")), 
+    linewidth = 1.05
+  ) +
+  scale_color_manual(values = wes_chevalier) +
+  coord_flip(xlim = c(0.5, 1.85),
+             ylim = c(0, 380000), expand = TRUE) +
+  guides(color = "none") +
+  labs(title = "Legend") +
+  custom_style3()+
+  theme_void(base_family = "Sen")+ 
+  theme(plot.title = element_text(family = "Sen", size = 12,
+                                  hjust = 0.05),
+        plot.background = element_rect(color = "grey30",
+                                       size = 0.2))
+
+
+
+
+
+## plot density plot for a given postion stratified by asset size. uses stat_halfeye from ggdist. 
+plot_dist <- function(y){
+  
+  dist_plot <- final_comp %>% 
+    fsubset(title == common_roles$title[y] &
+              !is.na(asset2) &
+              rep_comp < qx_calc(1, z=995)) %>%
+    fselect(asset2, rep_comp, org_name, label) %>% 
+    fmutate(med_comp = median(rep_comp)) %>% 
+    ggplot(aes(y = rep_comp, x = fct_rev(asset2)))+
+    stat_halfeye(alpha = 0.5)+
+    stat_interval()+
+    stat_summary(geom = "point", fun = median, size =3) +
+    geom_hline(aes(yintercept = med_comp), lty = "dashed")+
+    custom_style3()+
+    scale_y_continuous(labels = scales::dollar,
+                       name = "Median wage")+
+    scale_x_discrete(name = NULL,
+                     labels = function(i) custom_lbl_wrap(i,w=19))+
+    scale_color_manual(values = wes_chevalier)+
+    labs(title = paste0("Compensation Distribution for ",
+                       common_roles$title[y], "s ", 
+                       "by Organization Asset Size"), 
+         caption = "Note: This graph doesn't show the top 0.5% of earners.\nSource: Internal Revenue Service 2024")+
+    coord_flip(clip = 'off')+
+    theme(panel.grid.major.x = element_line(color = "gray"), 
+          axis.title = element_text(family = "Sen", size = 13))
+  
+  
+  
+  dist_final <- dist_plot + inset_element(p_legend, l = 0.6, r = 1.0,
+                                            t = 0.99, b = 0.7, clip = FALSE)
+  
+  
+  dist_final
+  
+}
+
+
+us_geo <- tigris::states(class = "sf", cb = TRUE) %>% 
+  tigris::shift_geometry()
+
+
+
+## make the relevant data frame specific to the position selected. 
+make_df_rel <- function(y){
+  
+  df_rel <- final_comp %>%
+    fselect(title, rep_comp, state, avg_hrs) %>% 
+    fsubset(title == common_roles$title[y]) %>% 
+    fsubset(rep_comp < q999) %>% 
+    fgroup_by(state) %>% 
+    fsummarise(med_comp = median(rep_comp, na_rm = TRUE), 
+               q25 = quantile(rep_comp, probs = 0.25), 
+               q75 = quantile(rep_comp, probs = 0.75))
+  
+  
+  df_rel <- us_geo %>% 
+    rename(state = STUSPS) %>% 
+    left_join(df_rel, by = "state") %>% 
+    filter(state %in% c(state.abb, "DC", "PR")) %>% 
+    mutate(across(c("med_comp", "q25", "q75"), round))
+  
+  df_rel
+  
+  
+  
+}
+
+
+
+plot_by_state <- function(y){
+  
+  gg <- ggplot(make_df_rel(y),
+               aes(fill = med_comp)) + 
+    geom_sf_interactive(aes(tooltip = paste0(NAME,"<br>",
+                                             "Median compensation: ", scales::dollar(med_comp), "<br>", 
+                                             "Half earn between: ", scales::dollar(q25), " & ",
+                                             scales::dollar(q75)),
+                            data_id = NAME), 
+                        size = 0.1) + 
+    scale_fill_gradientn(colors = my_pal2, labels = scales::label_dollar())+
+    labs(title = paste0("Median Compensation for ",
+                        common_roles$title[y], "s ", "by State"),
+         caption = "Note: This graph doesn't include the top 0.01% of earners.\nSource: Internal Revenue Service 2024",
+         fill = NULL) + 
+    custom_style3() +
+    theme(axis.text = element_blank(), 
+          legend.position = "top",
+          legend.justification = c("left", "top"),
+          legend.key.width = unit(4, "line")
+    ) 
+  
+  
+  
+  
+  girafe(ggobj = gg) %>%
+    girafe_options(opts_hover(css = "fill:cyan;"), 
+                   opts_zoom(min = 0.5, max = 4))
+  
+  
+  
+  
+  
+  
+}
+
+
+
+
+
+
+
+
+
+
+
